e8322cd41d23ae2accc80f7f1aaa0c80bf9d9386
librekcesar <140665209+librekcesar@users.noreply.github.com>
2025-07-16 17:43:32 -0600

    Add files via upload

---
+++ estaciones_mx0_spmx19_copia.js
@@ -0,0 +1,9 @@
+var estaciones = {
+"type": "FeatureCollection",
+"name": "estaciones_mx0_spmx19",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "MexiquenseParks, 54900, De las Torres 191, Sin Nombre, 54910 Fuentes del Valle, Méx.", "description": "MX0", "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1, "extrude": 0, "visibility": -1, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ -99.1476988, 19.6397536, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Pablo Veronés 410, Alfonso XIII, Álvaro Obregón, 01460 Ciudad de México, CDMX", "description": "SPMX19", "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1, "extrude": 0, "visibility": -1, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ -99.1940397, 19.3775755, 0.0 ] } }
+]
+}
---
+++ nombre_RUTA_MX0_SPMX19_copia.js
@@ -0,0 +1,8 @@
+var mx0_spmx19 = {
+"type": "FeatureCollection",
+"name": "nombre_RUTA_MX0_SPMX19",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "RUTA_MX0-SPMX19", "layer": "ruta_mx0_spmx19_simpl", "no_paradas": 1, "t_ruta": "4 Hrs", "t_riesgo": "Alto", "nombre": "RUTA_MX0_SPMX19", "CPS": 3.21, "path": "LineStringZ?crs=EPSG:4326&field=Name:string(0,0)&field=description:string(0,0)&field=timestamp:string(0,0)&field=begin:string(0,0)&field=end:string(0,0)&field=altitudeMode:string(0,0)&field=tessellate:integer(0,0)&field=extrude:integer(0,0)&field=visibility:integer(0,0)&field=drawOrder:string(0,0)&field=icon:string(0,0)&field=layer:string(0,0)&field=path:string(0,0)&uid={68ee2e9f-d2a8-47df-9b98-d0fad519acd6}", "t_costo": "Bajo" }, "geometry": { "type": "LineString", "coordinates": [ [ -99.1477, 19.63975, 0.0 ], [ -99.19003, 19.6386, 0.0 ], [ -99.19996, 19.63597, 0.0 ], [ -99.19458, 19.63584, 0.0 ], [ -99.18866, 19.60082, 0.0 ], [ -99.20135, 19.59552, 0.0 ], [ -99.20166, 19.5888, 0.0 ], [ -99.20811, 19.57526, 0.0 ], [ -99.20632, 19.55296, 0.0 ], [ -99.23667, 19.50874, 0.0 ], [ -99.23672, 19.48856, 0.0 ], [ -99.22033, 19.45703, 0.0 ], [ -99.22007, 19.44556, 0.0 ], [ -99.19643, 19.42179, 0.0 ], [ -99.19355, 19.41475, 0.0 ], [ -99.1942, 19.40859, 0.0 ], [ -99.18843, 19.39358, 0.0 ], [ -99.19182, 19.37812, 0.0 ], [ -99.19404, 19.37758, 0.0 ] ] } }
+]
+}
